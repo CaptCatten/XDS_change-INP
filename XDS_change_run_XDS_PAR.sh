401d85478db6f4da72f55ec14424e969d8d77ea3
CaptCatten <143093436+CaptCatten@users.noreply.github.com>
2025-11-18 16:14:27 +0100

    Update XDS_change_run_XDS_PAR.sh

--- XDS_change_run_XDS_PAR.sh
+++ XDS_change_run_XDS_PAR.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-ROOT="/media/lauren/T7/trim72_XDS_test"
-PYTHON_SCRIPT="/media/lauren/T7/trim72_XDS_test/XDS_mod.py"   # <-- CHANGE THIS TO THE REAL LOCATION
+# Use an absolute path to avoid some funny business. (I am sorry this is all I know how to do) 
+ROOT="/media/lauren/T7/trim72_XDS_test" # Change this to where your XDS.INP is
+PYTHON_SCRIPT="/media/lauren/T7/trim72_XDS_test/XDS_mod.py"   # Change to the absolute path of the python script location
 
 echo "=== Starting full pipeline ==="
 echo "Root directory: $ROOT"
